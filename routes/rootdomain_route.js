e9a42b619654c76993fe768e16c69a2b8fd872f8
Derek Bruinooge <derek.bruinooge@gmail.com>
2023-01-22 21:00:55 -0500

    Version 1

---
+++ routes/rootdomain_route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.js');
+
+router.get('/', async (req, res, next) => {
+  let allUsers = await User.find({});
+
+  return res.render('rootdomain', {
+    title: 'Accessing: ' + req.vhost.hostname,
+    allUsers: allUsers.map((user) => {
+      return {
+        ...user._doc,
+        link: 'https://' + user.link,
+        fullname: user.fullname,
+      };
+    }),
+  });
+});
+
+router.post('/', async (req, res) => {
+  try {
+    let data = {
+      email: req.body.email,
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    };
+
+    let user = new User(data);
+    await user.save();
+    return res.redirect('/');
+  } catch (error) {
+    return res.json({ ...error });
+  }
+});
+
+module.exports = router;
